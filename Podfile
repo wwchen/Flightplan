3e63cbe29d190bbba8aa2d17c2d423d620228cf7
William Chen <wichen@microsoft.com>
2014-08-05 11:17:23 -0700

    Pod init with MMDrawerController installed

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, "7.0"
+
+target "Flightplan" do
+  pod 'MMDrawerController', '~> 0.5.6'
+end
+
+target "FlightplanTests" do
+
+end
+
---
+++ Pods/Pods-Flightplan-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Flightplan : NSObject
+@end
+@implementation PodsDummy_Pods_Flightplan
+@end
